149d96bba64734b27afce6b81321571ea8b3f245
flowrina <emergencyemailusa@gmail.com>
2025-12-26 14:56:43 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
-// Fake online users counter (auto change)
-let users = document.getElementById("users");
-if (users) {
-  setInterval(() => {
-    let random = Math.floor(Math.random() * 10);
-    users.innerText = 120 + random;
-  }, 3000);
-}
-
-// Login button
-function enterChat() {
-  let name = document.getElementById("username").value;
-  if (name.length < 2) {
-    alert("Please enter a nickname");
-    return;
-  }
-  window.location.href = "index.html";
+function openRoom(room) {
+  const rooms = {
+    general: "lesbianhotgeneral",
+    dating: "lesbianhotdating",
+    friends: "lesbianhotfriends"
+  };
+  document.getElementById("chatFrame").src =
+    "https://chatango.com/embed/group?handle=" + rooms[room] + "&arch=js&styles=1";
 }
